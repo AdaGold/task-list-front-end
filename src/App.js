c2691a121a472f8993102d383952bd98995790b5
Chris McAnally <chris@adadevelopersacademy.org>
2021-11-14 18:41:12 -0800

    App.js for basic app with fixed data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import TaskList from './components/task_list';
+
+const TASKS = [
+  {
+    id: 1,
+    text: 'Mow the lawn',
+    done: false,
+  },
+  {
+    id: 2,
+    text: 'Cook Pasta',
+    done: true,
+  },
+];
 
 const App = () => {
+  const [tasks, setTasks] = useState(TASKS);
+  console.log(tasks);
+
+  const updateTask = (taskId) => {
+    console.log('Updating Task');
+    const newTasks = tasks.map((task) => {
+      if (task.id === taskId) {
+        task.done = !task.done;
+      }
+      return task;
+    });
+    setTasks(newTasks);
+  };
+
+  const deleteTask = (taskId) => {
+    const newTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(newTasks);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Ada&apos;s Task List</h1>
       </header>
-      <main>Main content</main>
+      <main>
+        <div>
+          <TaskList
+            tasks={TASKS}
+            onTaskClickCallback={updateTask}
+            onTaskDeleteCallback={deleteTask}
+          />
+        </div>
+      </main>
     </div>
   );
 };
