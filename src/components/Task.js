97abf733c565afab52368e51676fa28e4cbe8944
Chris McAnally <chris@adadevelopersacademy.org>
2021-12-02 13:51:06 -0800

    make Task.js stateful without callbacks

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,24 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import './Task.css';
 
-const Task = ({ id, text, done, onClickCallback, onDeleteCallback }) => {
-  const buttonClass = done ? 'tasks__item__toggle--completed' : '';
+const Task = ({ id, text, done }) => {
+  const [isDone, setIsDone] = useState(done);
+  const buttonClass = isDone ? 'tasks__item__toggle--completed' : '';
 
   return (
     <li className="tasks__item">
       <button
         className={`tasks__item__toggle ${buttonClass}`}
-        onClick={() => onClickCallback(id)}
+        onClick={() => setIsDone(!isDone)}
       >
         {text}
       </button>
       <button
         className="tasks__item__remove button alert pull-right"
-        onClick={() => onDeleteCallback(id)}
         data-testid={`delete button ${id}`}
       >
         <i className="fa fa-times">
@@ -33,8 +33,6 @@ Task.propTypes = {
   id: PropTypes.number.isRequired,
   text: PropTypes.string.isRequired,
   done: PropTypes.bool.isRequired,
-  onClickCallback: PropTypes.func.isRequired,
-  onDeleteCallback: PropTypes.func.isRequired,
 };
 
 export default Task;
