59d36fc0d00348fd2ccace8f9ada75d92fad5577
claire <yue.katou@gmail.com>
2022-12-12 15:45:12 -0800

    removes state and click events

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,20 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import './Task.css';
 
 const Task = ({ id, title, isComplete }) => {
-  const [complete, setComplete] = useState(isComplete);
-  const buttonClass = complete ? 'tasks__item__toggle--completed' : '';
-
   return (
     <li className="tasks__item">
-      <button
-        className={`tasks__item__toggle ${buttonClass}`}
-        onClick={() => setComplete(!complete)}
-      >
-        {title}
-      </button>
+      <button className="tasks__item__toggle">{title}</button>
       <button className="tasks__item__remove button">x</button>
     </li>
   );
