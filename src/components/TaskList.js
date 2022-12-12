3319cbc31c9921858b2f2d85424b4bd8be024dc6
claire <yue.katou@gmail.com>
2022-12-12 15:44:38 -0800

    modifies the getTaskListJSX function to look more like the resources on Learn props lesson

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -4,19 +4,17 @@ import Task from './Task';
 import './TaskList.css';
 
 const TaskList = ({ tasks }) => {
-  const getTaskListJSX = (tasks) => {
-    return tasks.map((task) => {
-      return (
-        <Task
-          key={task.id}
-          id={task.id}
-          title={task.title}
-          isComplete={task.isComplete}
-        />
-      );
-    });
-  };
-  return <ul className="tasks__list no-bullet">{getTaskListJSX(tasks)}</ul>;
+  const getTaskListJSX = tasks.map((task) => {
+    return (
+      <Task
+        key={task.id}
+        id={task.id}
+        title={task.title}
+        isComplete={task.isComplete}
+      />
+    );
+  });
+  return <ul className="tasks__list no-bullet">{getTaskListJSX}</ul>;
 };
 
 TaskList.propTypes = {
