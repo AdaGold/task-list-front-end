2f11619f562cca02ff14a7011c65ac6e7e52e3db
Mikelle Wade <mikelle.wade@gadadevelopersacademy.org>
2024-05-02 20:57:10 -0400

    removed tasks from app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,6 @@ import TaskList from './components/TaskList.jsx';
 import './App.css';
 import tasksJson from './data/tasks.json';
 
-const TASKS = [
-  {
-    id: 1,
-    title: 'Mow the lawn',
-    isComplete: false,
-  },
-  {
-    id: 2,
-    title: 'Cook Pasta',
-    isComplete: true,
-  },
-];
-
 const App = () => {
   const [tasks, setTasks] = useState(tasksJson);
 
