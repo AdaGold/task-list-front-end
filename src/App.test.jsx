c683319318aafd243cc6d41e5a8ba8cb354b5dca
Mikelle Wade <mikelle.wade@gadadevelopersacademy.org>
2024-11-19 11:12:34 -0500

    remove extend-expect imports, add .env to gitignore, update package-lock.json

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import '@testing-library/jest-dom/extend-expect';
 
 test("renders Ada's Task List", () => {
   // Act
